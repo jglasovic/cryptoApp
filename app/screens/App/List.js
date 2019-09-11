4248bd6e9608c65839c89a67a7f22df01591b656
Jure Glasovic <jglasovic@gmail.com>
2019-09-11 10:16:05 +0200

    add search filter

--- app/screens/App/List.js
+++ app/screens/App/List.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { View, FlatList, SafeAreaView, Text } from 'react-native';
+import { View, FlatList, SafeAreaView, Text, TextInput } from 'react-native';
 import CoinCard from '../../components/CoinCard';
 import moment from 'moment';
 
 const List = ({ currency, popularCoins, allCoins }) => {
+  const [searchText, handleSearch] = useState('');
   const popularList = Object.keys(popularCoins).map(key => ({ ...popularCoins[key][currency], ...allCoins[key] }));
-  const allList = Object.keys(allCoins).map(key => allCoins[key]);
-  console.log(allList[10]);
+  const allList = Object.keys(allCoins)
+    .map(key => allCoins[key])
+    .filter(coin => coin.fullName.toLowerCase().indexOf(searchText.toLowerCase()) > -1);
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
       <View style={{ paddingHorizontal: 14 }}>
         <Text style={{ color: 'white' }}>Popular</Text>
-        <View style={{ marginBottom: 14, borderColor: 'white', borderWidth: 1, borderBottomWidth: 0 }}>
+        <View style={{ marginBottom: 14, marginTop: 5, borderColor: 'white', borderWidth: 1, borderBottomWidth: 0 }}>
           {popularList.map(coin => (
             <CoinCard
               key={coin.id}
@@ -23,6 +26,20 @@ const List = ({ currency, popularCoins, allCoins }) => {
           ))}
         </View>
         <Text style={{ color: 'white' }}>All Coins</Text>
+        <TextInput
+          style={{
+            marginVertical: 5,
+            paddingHorizontal: 5,
+            height: 25,
+            borderColor: 'white',
+            borderWidth: 1,
+            color: 'white',
+          }}
+          onChangeText={handleSearch}
+          value={searchText}
+          placeholder={'Search'}
+          placeholderTextColor="white"
+        />
         <View style={{ marginBottom: 30, borderColor: 'white', borderWidth: 1 }}>
           <FlatList
             data={allList}
